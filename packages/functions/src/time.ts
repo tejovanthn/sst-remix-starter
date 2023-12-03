cefc58a22f9a2a429ba87e43b4fd5f7d44858774
Tejovanth N <1077633+tejovanthn@users.noreply.github.com>
2023-12-03 11:49:35 +0530

    useSession throws an error

--- packages/functions/src/time.ts
+++ packages/functions/src/time.ts
@@ -2,9 +2,9 @@ import handler from "@runboi/core/handler";
 import protect from "@runboi/core/protect";
 
 export const main = handler(
-  // protect(
+  protect(
     async (event) => {
       return { time: event.requestContext.time }
     }
-  // )
+  )
 );
